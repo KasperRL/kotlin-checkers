23e54ff17b3578b7b1556e540c77a4b990776d59
Kasper Ligthart <kasper.ligthart@icloud.com>
2024-10-30 13:16:32 +0100

    refactor: got rid of yet unused property isKing

--- src/main/kotlin/piece/Piece.kt
+++ src/main/kotlin/piece/Piece.kt
@@ -7,13 +7,11 @@ import org.example.util.Position
  * Represents a piece on the board
  * @property color Color of the piece
  * @property position Position (x, y) of the piece
- * @property isKing True if the piece is a king
  * @property isCaptured True if the piece has been captured
  * @constructor
  */
 data class Piece(
     val color: Color,
     var position: Position,
-    val isKing: Boolean = false,
     var isCaptured: Boolean = false
 )
